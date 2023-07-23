7f16448e7b5531150c50114df7a98e61d828d555
Nataly-Naf <nat.spivak84@gmail.com>
2023-07-23 20:05:38 +0200

    Task-08

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,17 @@
-
+const formEl = document.querySelector('.login-form')
+console.log(formEl)
+formEl.addEventListener('submit', SubmitHandler);
+function SubmitHandler(event) {
+    event.preventDefault();
+    const formElements = event.currentTarget.elements;
+        const mail = formElements.email.value;
+    console.log(mail)
+    const password = formElements.password.value;
+    if (password.length ===0 || mail.length ===0) {
+        alert('всі поля повинні бути заповнені')
+    }
+        const formData = { mail, password }
+    console.log(formData);
+    event.currentTarget.reset();
+    
+}
