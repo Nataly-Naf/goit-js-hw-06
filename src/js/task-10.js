2e16dec9ec3c6d11c97b9e732169e9291afaa625
Nataly-Naf <nat.spivak84@gmail.com>
2023-07-26 11:29:43 +0200

    Update task-10.js

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -3,3 +3,30 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const createBtn = document.querySelector('button[data-create]')
+const destroyBtn = document.querySelector('button[data-destroy]')
+const boxes = document.querySelector('#boxes')
+const inputEl = document.querySelector('input')
+console.dir (inputEl)
+
+
+const amount = inputEl.valueAsNumber;
+console.log(amount)
+function createBoxes(amount) {
+  for (let i = 1; i <= amount; i += 1) {
+    if (Number(inputEl.value) >= 1 && Number(inputEl.value) <= 100) {
+     
+          const divEl = document.createElement('div')
+      divEl.style.width = "30px";
+      divEl.style.height = "30px";
+      divEl.style.backgroundColor = getRandomHexColor();
+      boxes.append(divEl)
+    }
+  }
+}
+createBtn.addEventListener('submit', createBoxes(amount))
+destroyBtn.addEventListener('submit', cleanBoxes())
+function cleanBoxes() {
+  boxes.innerHTML=" "
+}
