8a519e160135b13c70413a24494f92dc4bde0186
Nataly-Naf <nat.spivak84@gmail.com>
2023-07-27 11:48:10 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -9,24 +9,29 @@ const destroyBtn = document.querySelector('button[data-destroy]')
 const boxes = document.querySelector('#boxes')
 const inputEl = document.querySelector('input')
 console.dir (inputEl)
-
-
-const amount = inputEl.valueAsNumber;
-console.log(amount)
-function createBoxes(amount) {
+const divArr = [];
+function onCreateBtnClick() {
+  const amount = inputEl.valueAsNumber;
+  console.log(amount)
   for (let i = 1; i <= amount; i += 1) {
-    if (Number(inputEl.value) >= 1 && Number(inputEl.value) <= 100) {
-     
-          const divEl = document.createElement('div')
-      divEl.style.width = "30px";
+    if (amount >= 1 && amount <= 100) {
+      const divEl = document.createElement('div')
+      console.log(divEl)
+          divEl.style.width = "30px";
       divEl.style.height = "30px";
       divEl.style.backgroundColor = getRandomHexColor();
-      boxes.append(divEl)
+      divArr.push(divEl);
+      console.log(divArr)
+          
     }
+    
   }
+  return boxes.append(...divArr)
+  
 }
-createBtn.addEventListener('submit', createBoxes(amount))
-destroyBtn.addEventListener('submit', cleanBoxes())
-function cleanBoxes() {
-  boxes.innerHTML=" "
+
+createBtn.addEventListener('click', onCreateBtnClick)
+destroyBtn.addEventListener('click', onDestroyBtnClick)
+function onDestroyBtnClick() {
+  return boxes.innerHTML=" "
 }
